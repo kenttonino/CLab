ee3fc02ffa8c567b35d1533d467a87d5b5312655
Kent Tonino <tonino.kentlouise@gmail.com>
2026-01-18 10:10:23 +0800

    feat: add a workflow

---
+++ src/ex_input_output/c_write_file.c
@@ -0,0 +1,12 @@
+#include "../helpers/helpers.h"
+#include <stdio.h>
+
+void c_write_file(void) {
+  // Program description.
+  printf("---------------------------------------");
+  new_line(1);
+  printf("Program: c_write_file.c");
+  new_line(1);
+  printf("---------------------------------------");
+  new_line(1);
+}
