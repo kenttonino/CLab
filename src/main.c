1ddcfdd98523acd7200084c097a95db97c3ebab8
Kent Tonino <tonino.kentlouise@gmail.com>
2026-01-25 13:17:34 +0800

    chore: commented sample

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
 #include "./helpers/helpers.h"
 #include "ex_concurreny/ex_concurrency.h"
-#include "ex_input_output/ex_input_output.h"
 
 int main(void) {
   // Cleanup the terminal.
@@ -10,8 +9,6 @@ int main(void) {
   // Below are the sample programs of different sections.
   ex_concurrency();
   new_line(2);
-  // ex_input_output();
-  // new_line(2);
 
   return 0;
 }
